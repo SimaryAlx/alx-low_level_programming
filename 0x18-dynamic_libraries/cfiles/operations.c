f7894d6ff1c37991474d5c3ecb76504f0bf182d2
lSimaryl <70992381+SimaryAlx@users.noreply.github.com>
2023-12-12 04:07:06 +0200

    Add files via upload

---
+++ 0x18-dynamic_libraries/cfiles/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+* add - calculates and add two numbers.
+* @a: firist number
+* @b: second number
+*
+* Return: addition.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* sub - calculates difference and subtract two numbers.
+* @a: firist number
+* @b: second number
+*
+* Return: subtracted final difference result.
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* mul - multiply and calculates product of two numbers
+* @a: firist number
+* @b: second number
+*
+* Return: product.
+*/
+int mul(int a, int b)
+{
+return ((a) * (b));
+}
+
+/**
+* div - divides and calculates the quotient of two numbers.
+* @a: firist number
+* @b: second number
+*
+* Return: division or quotient result.
+*/
+int div(int a, int b)
+{
+return ((a) / (b));
+}
+
+/**
+* mod - calculates teh modulus of two numbers
+* @a: firist number
+* @b: second number
+*
+* Return: final result of modulus.
+*/
+int mod(int a, int b)
+{
+return ((a) % (b));
+}
