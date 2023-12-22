2e43649a0b0467db0f2d73295b0fb73996d14ade
lSimaryl <70992381+SimaryAlx@users.noreply.github.com>
2023-12-23 00:06:24 +0200

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,51 @@
-#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *ht;
-	unsigned long int i;
+#define INITIAL_CAPACITY 10
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+typedef struct Node {
+    char *key;
+    char *value;
+    struct Node *next;
+} Node;
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+typedef struct HashTable {
+    unsigned long int size;
+    Node **array;
+} hash_table_t;
 
-	return (ht);
+hash_table_t *hash_table_create(unsigned long int size) {
+    hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+    if (hash_table == NULL) {
+        return NULL;  // Unable to allocate memory for the hash table
+    }
+
+    hash_table->size = size;
+    hash_table->array = (Node **)calloc(size, sizeof(Node *));
+    if (hash_table->array == NULL) {
+        free(hash_table);
+        return NULL;  // Unable to allocate memory for the array
+    }
+
+    return hash_table;
+}
+
+int main() {
+    unsigned long int size = INITIAL_CAPACITY;
+    hash_table_t *my_hash_table = hash_table_create(size);
+
+    if (my_hash_table == NULL) {
+        fprintf(stderr, "Error creating hash table\n");
+        return 1;
+    }
+
+    printf("Hash table created successfully with size %lu\n", my_hash_table->size);
+
+    // Perform other operations on the hash table as needed
+
+    // Don't forget to free the allocated memory when done
+    // free(my_hash_table->array);
+    // free(my_hash_table);
+
+    return 0;
 }
